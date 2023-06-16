113679abfc6aa3a100f3f9e653a92fbd895920eb
Marcelo Motta <marcelo.motta@gmail.com>
2023-06-15 21:57:56 -0300

    Responsivo e dark mode

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-let main = document.getElementById('main')
+const main = document.getElementById('main')
+const loading = document.getElementById('loading')
+const prevLink = document.getElementById('prev')
+const nextLink = document.getElementById('next')
+const searchEl = document.getElementById('search')
+const search_buttonEl = document.getElementById('search_button')
+const resultsEl = document.getElementById('results')
+const per_pageEl = document.getElementById('per_page')
+const pokemon_info_dialog = document.getElementById('pokemon_info')
+const pokemon_info_datailEl = document.getElementById('pokemon_info_datail')
+
+
+const darkmode = document.getElementById('darkmode')
+darkmode.addEventListener('click', (event) => {
+    document.body.className = event.target.value
+    event.target.innerText = event.target.value == 'dark' ? 'light' : 'dark'
+    event.target.value = event.target.value == 'dark' ? 'light' : 'dark'
+})
+
 let pokemons = []
+let pokemons_filtered = []
 let page = 1
 let offset = 0
-let limit = 6
-let prevLink = document.getElementById('prev')
-let nextLink = document.getElementById('next')
+let limit = 3
 
 String.prototype.capitalize = function () { return this.charAt(0).toUpperCase() + this.substr(1); }
 
-function fetchApi(url) {
-    return fetch(`https://pokeapi.co/api${url}?limit=${limit}&offset=${offset}`)
+function fetchApi(url, infinite = false) {
+    return fetch(`https://pokeapi.co/api${url}?limit=${infinite ? 1281 : limit}&offset=${offset}`)
         .then((res) => res.json())
 }
 
 function getSpriteUrl(id) {
-    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png`
 }
 
 prevLink.addEventListener('click', (event) => {
@@ -30,59 +47,131 @@ nextLink.addEventListener('click', (event) => {
     fetchPokemon()
 })
 
+per_pageEl.addEventListener('change', (event) => {
+    limit = event.target.value
+    fetchPokemon()
+})
+
+// searchEl.addEventListener('keydown', (event) => {
+//     if (event.code == 'Enter') {
+//         searchPokemon(event.target.value)
+//     }
+// })
+// search_buttonEl.addEventListener('click', () => {
+//     if (searchEl.value.length > 0) {
+//         searchPokemon(searchEl.value)
+//     }
+// })
+
+searchEl.addEventListener('keyup', (event) => {
+    pokemons_filtered = pokemons.filter((pokemon) => pokemon.name.includes(event.target.value))
+    resultsEl.innerHTML = ''
+    resultsEl.style.display = 'flex'
+    pokemons_filtered.forEach((pokemon, index) => {
+        if (index < 5) {
+            let label = document.createElement('span')
+            label.innerText = pokemon.name
+            label.setAttribute('data-pokemon', pokemon.name)
+            resultsEl.appendChild(label)
+
+            label.addEventListener('click', (event) => {
+                searchPokemon(event.target.dataset.pokemon)
+                clearSearch()
+            })
+        }
+    })
+    if (event.target.value.length == 0) {
+        clearSearch()
+    }
+})
+
+function clearSearch() {
+    resultsEl.innerHTML = ''
+    resultsEl.style.display = 'none'
+    pokemons_filtered = []
+}
+
 fetchPokemon()
+fetchApi(`/v2/pokemon`, true)
+    .then((data) => {
+        pokemons = data.results
+    })
 
 function fetchPokemon() {
-
+    main.style.display = 'none'
+    loading.style.display = 'flex'
     main.innerHTML = ''
-
     fetchApi(`/v2/pokemon`)
         .then((data) => {
-            pokemons = data.results
+            renderPokemons(data.results)
         })
-        .then(() => {
+}
 
-            pokemons.forEach((pokemon, index) => {
+function searchPokemon(name) {
+    main.innerHTML = ''
+    fetchApi(`/v2/pokemon/${name}`)
+        .then((data) => {
+            renderPokemons([data])
+        })
+        .catch(err => {
+            alert('Pokemon não localizado')
+        })
+}
 
-                fetchApi(`/v2/pokemon/${pokemon.name}`)
-                    .then((detail) => {
+function renderPokemons(pokemons = []) {
+    pokemons.forEach((pokemon, index) => {
 
-                        let card = document.createElement('div')
-                        card.className = 'card'
+        fetchApi(`/v2/pokemon/${pokemon.name}`)
+            .then((detail) => {
 
-                        let avatar = document.createElement('div')
-                        avatar.className = 'avatar'
-                        avatar.style.backgroundImage = `url(${getSpriteUrl(detail.id)})`
+                let card = document.createElement('div')
+                card.className = 'card'
 
-                        card.appendChild(avatar)
+                let avatar = document.createElement('div')
+                avatar.className = 'avatar'
+                avatar.style.backgroundImage = `url(${getSpriteUrl(detail.id)})`
 
-                        let info = document.createElement('div')
-                        info.className = 'info'
+                card.appendChild(avatar)
 
-                        card.appendChild(info)
+                let info = document.createElement('div')
+                info.className = 'info'
 
-                        let types = document.createElement('div')
-                        types.className = 'types'
+                card.appendChild(info)
 
-                        info.appendChild(types)
+                let types = document.createElement('div')
+                types.className = 'types'
 
-                        detail.types.forEach((t) => {
-                            let badge = document.createElement('span')
-                            badge.classList = `badge badge-${t.type.name}`
-                            badge.innerText = t.type.name
-                            types.appendChild(badge)
-                        })
+                info.appendChild(types)
 
-                        let name = document.createElement('div')
-                        name.className = 'name'
-                        name.innerText = pokemon.name.capitalize()
+                detail.types.forEach((t) => {
+                    let badge = document.createElement('span')
+                    badge.classList = `badge badge-${t.type.name}`
+                    badge.innerText = t.type.name
+                    types.appendChild(badge)
+                })
 
-                        info.appendChild(name)
-                        main.appendChild(card)
-                    })
+                let name = document.createElement('div')
+                name.className = 'name'
+                name.innerText = pokemon.name.capitalize()
 
+                info.appendChild(name)
+                main.appendChild(card)
+
+                avatar.addEventListener('click', (event) => {
+                    pokemon_info_dialog.showModal()
+
+                    pokemon_info_datailEl.innerText = ''
+
+                    let pokemon_name = document.createElement('span')
+                    pokemon_name.innerText = pokemon.name.capitalize()
+                    pokemon_info_datailEl.append(pokemon_name)
+                })
+            })
+            .then(() => {
+                main.style.display = 'grid'
+                loading.style.display = 'none'
             })
 
-        })
+    })
 }
 
